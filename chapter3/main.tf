49a26803f49e4a6706aadfc7b2ec77d0714c4ae6
Ahmed Mansour <ahmedsmansour@proton.me>
2024-05-03 11:34:59 +0300

    Created unique bucket name

--- chapter3/main.tf
+++ chapter3/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "terraform-state-123"
+  bucket = "terraform-state-1234455"
 
   lifecycle {
     prevent_destroy = true
@@ -26,4 +26,23 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
     }
   }
   
+}
+
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket = aws_s3_bucket.terraform_state.id
+  block_public_acls = true
+  block_public_policy = true
+  ignore_public_acls = true
+  restrict_public_buckets = true
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name = "terraform-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key = "LockID"
+  
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
 }
