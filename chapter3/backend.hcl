4c53a9d8b910349fa6860d40756981261f1b9f82
Ahmed Mansour <ahmedsmansour@proton.me>
2024-05-12 13:05:56 +0300

    Adjusted file structure

--- chapter3/backend.hcl
+++
@@ -1,4 +0,0 @@
-bucket = "terraform-state-123654"
-region = "us-east-2"
-dynamodb_table = "terraform-locks"
-encrypt = true
---
+++ chapter3/global/s3/variables.tf
@@ -0,0 +1,11 @@
+variable "table_name" {
+  description = "The name of the ALB"
+  type        = string
+  default     = "terraform-locks"
+}
+
+variable "bucket_name" {
+  description = "The name of the ALB"
+  type        = string
+  default     = "terraform-state-123654"
+}
