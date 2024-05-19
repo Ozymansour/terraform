4723f7851a26c7f223020a2c8515f1b3205eee48
Ahmed Mansour <ahmedsmansour@proton.me>
2024-05-19 12:35:37 +0300

    Updated source URL in stage environment

--- chapter4/modules/services/webserver-cluster/main.tf
+++ chapter4/modules/services/webserver-cluster/main.tf
@@ -15,7 +15,7 @@ resource "aws_launch_configuration" "example" {
   security_groups = [aws_security_group.instance.id]
 
   # Render the User Data script as a template
-  user_data = templatefile("user-data.sh", {
+  user_data = templatefile("${path.module}/user-data.sh", {
     server_port = var.server_port
     db_address  = data.terraform_remote_state.db.outputs.address
     db_port     = data.terraform_remote_state.db.outputs.port
@@ -114,22 +114,29 @@ resource "aws_lb_listener_rule" "asg" {
 
 resource "aws_security_group" "alb" {
   name = "${var.cluster_name}-alb"
+}
 
+resource "aws_security_group_rule" "allow_http_inbound" {
   # Allow inbound HTTP requests
-  ingress {
-    from_port   = local.http_port
-    to_port     = local.http_port
-    protocol    = local.tcp_protocol
-    cidr_blocks = local.all_ips
-  }
+  type = "ingress"
+  security_group_id = aws_security_group.alb.id
+  
+  from_port   = local.http_port
+  to_port     = local.http_port
+  protocol    = local.tcp_protocol
+  cidr_blocks = local.all_ips
 
-  # Allow all outbound requests
-  egress {
+}
+
+
+resource "aws_security_group_rule" "allow_all_outbound" {
+  type = "egress"
+  security_group_id = aws_security_group.alb.id
     from_port   = local.any_port
     to_port     = local.any_port
     protocol    = local.any_protocol
     cidr_blocks = local.all_ips
-  }
+  
 }
 
 data "terraform_remote_state" "db" {
--- chapter4/modules/services/webserver-cluster/outputs.tf
+++ chapter4/modules/services/webserver-cluster/outputs.tf
@@ -7,3 +7,8 @@ output "asg_name" {
   value = aws_autoscaling_group.example.name
   description = "Name of ASG"
 }
+
+output "alb_security_group_id" {
+  value = aws_security_group.alb.id
+  description = "ID of SG attatched to load balancer"
+}
