8277585d1f6ab286ad2f3176103f3601a5272592
Ahmed Mansour <ahmedsmansour@proton.me>
2024-06-17 12:34:00 +0300

    Added for_each loop

--- chapter5/live/global/iam/outputs.tf
+++ chapter5/live/global/iam/outputs.tf
@@ -1,5 +1,7 @@
-output "user_arns" {
-  value = module.users[*].user_arn
-  description = "ARNS of created IAM users"
+output "all_arns" {
+  value = values(aws_iam_user.example)[*].arn
 }
 
+output "all_users" {
+  value = aws_iam_user.example
+}
